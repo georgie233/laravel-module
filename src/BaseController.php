a1e98266651db423d9fd793d27ad3c7b40688651
zhanruijia <227040015@qq.com>
2021-11-10 16:48:04 +0800

    v1.1

--- src/BaseController.php
+++ src/BaseController.php
@@ -7,11 +7,26 @@ use Illuminate\Http\Request;
 class BaseController extends Controller
 {
     //
+    protected $isAjax = false;
+    public function __construct()
+    {
+        if (request()->ajax()){
+            $this->isAjax = true;
+        }
+    }
+
     public function responseSuccess($data){
         return response()->json([
             'code' => 200,
             'message' => '成功',
             'data' => $data,
+        ], 200);
+    }
+    public function responseIllegal(){
+        return response()->json([
+            'code' => 403,
+            'message' => '非法访问',
+            'data' => [],
         ], 200);
     }
 }
